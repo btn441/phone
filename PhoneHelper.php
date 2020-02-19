efb5f3e1c9499ba3c68ee2658990cf56e73ba1c0
Maxat Zhunussov <maxat_krg@rocketfirm.com>
2020-02-19 18:47:28 +0600

    first commit

---
+++ PhoneHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\helpers;
+
+/**
+ * Class PhoneHelper
+ * @author Maxat Zhunussov <btn441@gmail.com>
+ */
+class PhoneHelper
+{
+    const PHONE_MASK = [
+            'kazakhstan' => '+7 (999) 999 99 99',
+            'russia' => '+\7 (999) 999 99 99',
+            'azerbaijan' => '+\9 (\9\49) 999 99 99',
+            'armenia' => '+\3 (\7\49) 999 99 99',
+            'belarus' => '+\3 (\7\59) 999 99 99',
+            'kyrgyzstan' => '+\9 (\9\69) 999 99 99',
+            'moldova' => '+\9 (999) 999 99 99',
+            'tajikistan' => '+\9 (\9\29) 999 99 99',
+            'uzbekistan' => '+\9 (\9\89) 999 99 99',
+        ];
+}
